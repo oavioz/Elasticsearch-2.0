59e8924f7ebe096392b218611c178805b0121f9f
oavioz <oshika@OshikA-PC.il.corp.radware.com>
2015-11-15 17:47:51 +0200

    Update Cookbook files

--- elasticsearch2.0-install.sh
+++ elasticsearch2.0-install.sh
@@ -16,38 +16,38 @@
    # Add the Oracle Java PPA to apt:
    #Note: If software-properties-common is not installed please run: apt-get -y install software-properties-common
 
-                        sudo add-apt-repository -y ppa:webupd8team/java
+		sudo add-apt-repository -y ppa:webupd8team/java
 
    Update your apt package database:
 
-                        sudo apt-get update
+		sudo apt-get update
 
    Install the latest stable version of Oracle Java 8 with this command (and accept the license agreement that pops up):
 
-                echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-                echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-                sudo apt-get -y install oracle-java8-installer
+		echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+		echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+		sudo apt-get -y install oracle-java8-installer
 
   # Install Elasticsearch 2.0
   #Elasticsearch can be installed with a package manager by adding Elastic's package source list.
 
         #Run the following command to import the Elasticsearch public GPG key into apt:
 
-                        wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+		wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
         #If your prompt is just hanging there, it is probably waiting for your user's password (to authorize the sudo command). If this is the case, enter your password.
 
         #Create the Elasticsearch source list:
 
-                        echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+		echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 
         #Update your apt package database:
 
-                        sudo apt-get update
+		sudo apt-get update
 
    #Install Elasticsearch with this command:
 
-                        sudo apt-get -y install elasticsearch
+		sudo apt-get -y install elasticsearch
 
    #Elasticsearch is now installed. Let's edit the configuration:
 
@@ -55,11 +55,11 @@
 
 
         #Now start Elasticsearch:
-                     sudo service elasticsearch restart
+		 sudo service elasticsearch restart
 
         #Then run the following command to start Elasticsearch on boot up:
 
-                        sudo update-rc.d elasticsearch defaults 95 10
+		sudo update-rc.d elasticsearch defaults 95 10
 
         # To check if Elasticsearch is up and running please run the command:
 
